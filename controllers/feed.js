78665c3d5d9d602d21c01d8e6ee345e7f5aeb30d
Choi GD <ChoiGD0307@gmail.com>
2023-02-06 17:20:43 +0900

    restAPI +React

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,16 +1,24 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title : 'First', content: "First post!!"}]
+        posts: [
+            {   
+                _id:'1',
+                title: 'First',
+                content: "First post!!",
+                imageUrl: 'images/duck.jpg',
+                creator: { name: 'ChoiGD' },
+                date: new Date()
+            }]
     })
 };
 
-exports.createPost = (req, res, next) =>{
+exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
 
     res.status(201).json({
-        message:'Post created successfully!',
-        post:{ id: new Date().toISOString(), title: title, content:content}
+        message: 'Post created successfully!',
+        post: { id: new Date().toISOString(), title: title, content: content }
     });
 
 };

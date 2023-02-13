13f311891f8718a7635fadb82c10e3f2c1d4742f
Choi GD <ChoiGD0307@gmail.com>
2023-02-13 15:20:49 +0900

    getList + getOneList

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,16 +3,15 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'First',
-                content: "First post!!",
-                imageUrl: 'images/duck.jpg',
-                creator: { name: 'ChoiGD' },
-                date: new Date()
-            }]
+    Post.find()
+    .then(posts =>{
+        res.status(200).json({message: 'Fetched posts successfully.', posts: posts})
+    })
+    .catch(err =>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
     })
 };
 
@@ -48,3 +47,23 @@ exports.createPost = (req, res, next) => {
         })
 
 };
+
+exports.getPost = (req, res, next) =>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post =>{
+            if(!post){
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({message: 'Post fetched.', post: post})
+        })
+        .catch(err =>{
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+
+        })
+}
